fc924aa7cba8df22f7f0c0aa61fb54aead61014a
Alex <lexanus686@gmail.com>
2019-12-19 16:34:48 +0300

    Here I added in src types.ts (for buttons interface) and init.ts (for adding them after any action on screen) Modified extension.ts for adding buttons after try to load Hello world in terminal

--- src/extension.ts
+++ src/extension.ts
@@ -2,26 +2,41 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+//For button
+import init from './init';
+
+// import { TreePlaylistProvider } from './components/tree-playlists';
+// import { TreeTrackProvider } from './components/tree-track';
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vspooky" is now active!');
+	console.log('Congratulations, your extension "spotifyplayer" is now active!');
+
+	//For buttons
+	init(context);
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('extension.refreshButtons', () => {
 		// The code you place here will be executed every time your command is executed
 
+		//For buttons
+		init(context);
+
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+		// vscode.window.showInformationMessage('Hello World!');
+
+		//Use it for creating new treeView
+		// const playlistTreeView = vscode.window.createTreeView('vscode-spotify-playlists', { treeDataProvider: new TreePlaylistProvider() });
+    	// const treeTrackProvider = new TreeTrackProvider();
+    	// const trackTreeView = vscode.window.createTreeView('vscode-spotify-tracks', { treeDataProvider: treeTrackProvider });
+		// treeTrackProvider.bindView(trackTreeView);
 	});
 
 	context.subscriptions.push(disposable);
 }
-
-// this method is called when your extension is deactivated
-export function deactivate() {}
---
+++ src/init.ts
@@ -0,0 +1,63 @@
+// import { buildConfigFromPackageJson } from './packageJson'
+import * as vscode from 'vscode';
+import { RunButton } from './types';
+import * as path from 'path';
+
+const registerCommand = vscode.commands.registerCommand;
+
+const disposables: any[] = [];
+
+const init = async (context: vscode.ExtensionContext) => {
+	disposables.forEach(d => d.dispose());
+	const config = vscode.workspace.getConfiguration('actionButtons');
+	const defaultColor = config.get<string>('defaultColor');
+	const reloadButton = config.get<string>('reloadButton');
+	const playButton = config.get<string>('playButton');
+	const pauseButton = config.get<string>('pauseButton');
+
+	if (reloadButton !== null) {
+		loadButton({
+			vsCommand: 'extension.refreshButtons',
+			name: reloadButton || '↻',
+			color: defaultColor || 'white',
+			command: 'Refreshes the action buttons'
+		});
+	}
+
+	if (playButton !== null) {
+		loadButton({
+			vsCommand: 'extension.playButtons',
+			name: reloadButton || '←',
+			color: defaultColor || '#ff9900',
+			command: 'Plays the action buttons'
+		});
+	}
+
+	if (pauseButton !== null) {
+		loadButton({
+			vsCommand: 'extension.pauseButtons',
+			name: reloadButton || '||',
+			color: defaultColor || '#99ff00',
+			command: 'Stops the action buttons'
+		});
+	}
+	
+};
+
+function loadButton({
+	command,
+	name,
+	color,
+	vsCommand
+}: RunButton) {
+	const runButton = vscode.window.createStatusBarItem(1, 0);
+	runButton.text = name;
+	runButton.color = color || 'white';
+	runButton.tooltip = command;
+
+	runButton.command = vsCommand;
+	runButton.show();
+	disposables.push(runButton);
+}
+
+export default init;
